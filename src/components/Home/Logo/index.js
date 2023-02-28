b42e4798c2ef2d97ccfd7da0f0cd49883fee242a
Edmar Radanovis <edradanovis@gmail.com>
2023-02-28 11:50:24 -0300

    Uninstall npm gsap-trial

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,8 +1,9 @@
 import "./index.scss";
 import LogoS from "../../../assets/images/logo-vert1.png";
-import { useEffect, useRef } from "react";
-import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin";
-import gsap from "gsap-trial";
+import { useRef } from "react";
+// import { useEffect, useRef } from "react";
+// import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin";
+// import gsap from "gsap-trial";
 
 const Logo = () => {
   const bgRef = useRef();
@@ -10,58 +11,58 @@ const Logo = () => {
   const outlineLogoRef2 = useRef();
   const solidLogoRef = useRef();
 
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin);
+  // useEffect(() => {
+  //   gsap.registerPlugin(DrawSVGPlugin);
 
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 2,
-        duration: 20,
-      });
+  //   gsap
+  //     .timeline()
+  //     .to(bgRef.current, {
+  //       duration: 1,
+  //       opacity: 1,
+  //     })
+  //     .from(outlineLogoRef.current, {
+  //       drawSVG: 2,
+  //       duration: 20,
+  //     });
 
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    );
-  }, []);
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin);
+  //   gsap.fromTo(
+  //     solidLogoRef.current,
+  //     {
+  //       opacity: 0,
+  //     },
+  //     {
+  //       opacity: 1,
+  //       delay: 4,
+  //       duration: 4,
+  //     }
+  //   );
+  // }, []);
+  // useEffect(() => {
+  //   gsap.registerPlugin(DrawSVGPlugin);
 
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef2.current, {
-        drawSVG: 4,
-        duration: 20,
-      });
+  //   gsap
+  //     .timeline()
+  //     .to(bgRef.current, {
+  //       duration: 1,
+  //       opacity: 1,
+  //     })
+  //     .from(outlineLogoRef2.current, {
+  //       drawSVG: 4,
+  //       duration: 20,
+  //     });
 
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    );
-  }, []);
+  //   gsap.fromTo(
+  //     solidLogoRef.current,
+  //     {
+  //       opacity: 0,
+  //     },
+  //     {
+  //       opacity: 1,
+  //       delay: 4,
+  //       duration: 4,
+  //     }
+  //   );
+  // }, []);
 
   return (
     <div className="logo-container" ref={bgRef}>
